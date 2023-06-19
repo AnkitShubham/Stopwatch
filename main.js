5d5c96b6e055a9d9766197d9c1a08f2e72412156
Ankit Shubham <ankitshubham2002@gmail.com>
2023-06-19 16:47:58 +0530

    added: flag feature

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const startBtn = document.getElementById("start");
 const stopBtn = document.getElementById("stop");
+const flagbtn = document.getElementById("flag");
 const resetBtn = document.getElementById("reset");
 
 let millisecond = document.getElementById("milliseconds");
@@ -12,9 +13,14 @@ let elapsedTime = 0;
 let intervalID;
 
 
+let [ms, sec, min, hr] = [0, 0, 0, 0];
+
+
 startBtn.addEventListener('click',startStopwatch);
 
 stopBtn.addEventListener('click',stopStopwatch);
+
+flagbtn.addEventListener('click',record);
 
 resetBtn.addEventListener('click',resetStopwatch);
 
@@ -42,13 +48,28 @@ function updateStopwatch(){
 }
 
 function updateDisplay(){
-    let ms = Math.floor((elapsedTime % 1000)/10);
-    let sec = Math.floor((elapsedTime % 60000)/1000);
-    let min = Math.floor((elapsedTime % 3600000/60000));
-    let hr = Math.floor((elapsedTime / 3600000));
+    ms = Math.floor((elapsedTime % 1000)/10);
+    sec = Math.floor((elapsedTime % 60000)/1000);
+    min = Math.floor((elapsedTime % 3600000/60000));
+    hr = Math.floor((elapsedTime / 3600000));
 
     millisecond.innerHTML = ms;
     second.innerHTML = sec;
     minute.innerHTML = min;
     hour.innerHTML = hr;
+}
+
+function record(){
+    let tempMs = ms;
+    let tempSec = sec;
+    let tempMin = min;
+    let tempHr = hr;
+
+
+    let tempFlag = tempHr+"hr : "+tempMin+"min : "+tempSec+"sec : "+tempMs+"ms ";
+
+
+    let flags = document.createElement('li');
+    flags.appendChild(document.createTextNode(tempFlag));
+    document.querySelector('ol').appendChild(flags);
 }
